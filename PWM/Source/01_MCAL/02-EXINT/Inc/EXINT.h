d4934b73d5eff7b6ad39434806dd7ca4b9bd0e92
Hana Fares <103067954+hanafares25@users.noreply.github.com>
2022-11-23 15:06:59 +0200

    Update EXINT.h

--- PWM/Source/01_MCAL/02-EXINT/Inc/EXINT.h
+++ PWM/Source/01_MCAL/02-EXINT/Inc/EXINT.h
@@ -9,8 +9,13 @@
 #include "Bit_Math.h"
 #include "EXINT_Reg.h"
 #include "EXINT_Types.h"
-void Ext_Interrupt_Enable(ext_Int_t intID);
-void Ext_Interrupt_Disable(ext_Int_t intID);
-void Ext_Interrupt_SncControl(ext_Int_t intID, snc_mode_t mdoe);
 
-#endif /* SOURCE_01_MCAL_02_EXINT_INC_EXINT_H_ */
+#define INT0_EN_BIT			(6)
+#define INT1_EN_BIT			(7)
+#define INT2_EN_BIT			(5)
+
+
+void ExtInt_vidConfigExtInt(extint_id_t intId, extint_sensectrl_t sensCtrl);
+void ExtInt_vidDisableExtInt(extint_id_t intId);
+
+#endif /* SOURCE_01_MCAL_EXTINT_INC_EXTINT_H_ */
